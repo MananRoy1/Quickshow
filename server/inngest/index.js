37cecb60282e811925a9bbed768f97f1a57f4542
Manan Roy <roymanan111@gmail.com>
2025-07-05 23:15:32 +0530

    env change

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -96,16 +96,20 @@ const sendBookingConfirmationEmail = inngest.createFunction(
       subject: `Payment Confirmation: "${booking.show.movie.title}" booked !`,
       body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
                    <h2>Hi ${booking.user.name},</h2>
-                   <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
+                   <p>Your booking for <strong style="color: #F84565;">"${
+                     booking.show.movie.title
+                   }"</strong> is confirmed.</p>
                    <p>
-                      <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString(
-                      "en-US",
-                       { timeZone: "Asia/Kolkata" }
-                     )}<br/>
-                      <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString(
-                      "en-US",
-                      { timeZone: "Asia/Kolkata" }
-                      )}
+                      <strong>Date:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleDateString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}<br/>
+                      <strong>Time:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleTimeString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}
                   </p>
                   <p>Enjoy the show! 🍿</p>
                   <p>Thanks for booking with us!<br/>- QuickShow Team</p>
@@ -114,10 +118,39 @@ const sendBookingConfirmationEmail = inngest.createFunction(
   }
 );
 
+// Inngest function to send notifications when a new show is Added
+const sendNewShowNotification = inngest.createFunction(
+  { id: "send-new-show-notification" },
+  { event: "app/show.added" },
+  async ({ event }) => {
+    const { movieTitle } = event.data;
+
+    const users = await User.find({});
+
+    for (const user of users) {
+      const userEmail = user.email;
+      const userName = user.name;
+
+      const subject = `🎞️ New Show Added: ${movieTitle}`;
+      const body = `<div>
+                      <h2>${userName}</h2>
+                    </div>`;
+
+      await sendEmail({
+        to: userEmail,
+        subject,
+        body,
+      });
+    }
+    return { message: "Notification sent." };
+  }
+);
+
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
   releaseSeatsAndDeleteBooking,
-  sendBookingConfirmationEmail
+  sendBookingConfirmationEmail,
+  sendNewShowNotification,
 ];
